a3390aa1e7d62e7179e51d6e1db4cc7741904960
Michal Berezanski <michal.berezanski@op.pl>
2016-12-17 11:05:51 +0100

    Added different ways of using Handlers

--- app/src/main/java/pl/akademiakodu/animation/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/animation/MainActivity.java
@@ -1,6 +1,8 @@
 package pl.akademiakodu.animation;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -10,10 +12,37 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView logo ;
     RelativeLayout layout;
+
+    // żeby obsłużyć w Handlerze musimy nasza animacje wyciagnac jako globalna zeby moc obsluzyc w wiadomosci Handlera
+    AnimationDrawable animationDrawable;
+
+    // Tworzymy Handlera
+    Handler handlerAnim = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what==1){
+                animationDrawable.start();
+                // musi byc main activity.this bo Handller jest globalny i trzeba wpisac layout gdyz tutaj samo this nie wystarczy
+                // po prostu nie widzi tego layoutu samo this
+                Toast.makeText(MainActivity.this, "Wiadomość nr 1", Toast.LENGTH_LONG).show();
+            } else if(msg.what==2){
+                animationDrawable.stop();
+                Toast.makeText(MainActivity.this, "Wiadomość nr 2", Toast.LENGTH_SHORT).show();
+            } else if (msg.what==3){// obslugujemy wiadomość
+                Bundle data = msg.getData();
+                // metoda getString key i coś tam zabezpiecza nas przed wyjatkiem nullPointerException jeśli wiadomość byłaby pusta
+                // wtedy wyświetli nam Brak (error)
+                // interfejs użytkownika możemy obsługiwać tylko z wątku głównego - ŻELAZNA ZASADA !!!!!!!!!!!
+                Toast.makeText(MainActivity.this, data.getString("msg", "Brak (error)"), Toast.LENGTH_LONG).show();
+            }
+            return false;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +53,36 @@ public class MainActivity extends AppCompatActivity {
         layout = (RelativeLayout) findViewById(R.id.activity_main);
 
         // musimy zrzutowac na animacje ponieważ sama metoda getBackground() zwraca wartosc typu tylko Drawable
-        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
-        animationDrawable.start();
+        animationDrawable = (AnimationDrawable) layout.getBackground();
+
+        // opóźniamy start animacji o 4s i po 4s ja zamykamy
+        handlerAnim.sendEmptyMessageDelayed(1, 4000);
+        handlerAnim.sendEmptyMessageDelayed(2, 8000);
+
+        // tworzymy pusty obiekt wiadomości
+        Message message = new Message();
+        message.what=3;
+
+        // klasa Bundle przechowuje nasze zdefiniwane informacje jako różne typy danych
+        Bundle data = new Bundle();
+        data.putString("msg", "Informacja przesłana wiadomością");
+
+        // ustawiamy do wiadomosci klasę Bundle
+        message.setData(data);
+
+        // wysyłamy wiadomość - nie pustą!
+        handlerAnim.sendMessageDelayed(message, 5000);
+
+        // szybsze stworzenie zadania do wykonania z opóźnieniem
+        //new Handler().postDelayed(new Runnable() {
+           // @Override
+            //public void run() {
+                // jakieś coś wykona się po 3 sekundach
+            //}
+       // })
 
         // Załadowanie animacji - przypisanie
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fadein);
         //animation.setFillAfter(true); // pozostawienie widzetu w stanie po animacji
 
         // ANIMACJA Z KODU !!!
@@ -55,6 +109,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Uruchomienie animacji
         //logo.startAnimation(scaleAnimation);
-        logo.startAnimation(animation);
+        //logo.startAnimation(animation);
     }
 }
